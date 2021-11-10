49152847efbb7e4a2488477f746041ab256b079f
RodericAguilar <raguilar@elpuig.xeill.net>
2021-11-10 20:10:25 +0100

    Update ConfigSv.lua

--- Server/ConfigSv.lua
+++ Server/ConfigSv.lua
@@ -1,6 +1,6 @@
 ConfigSv = {}
 
-ConfigSv.Webhook = 'https://discord.com/api/webhooks/904358906640351283/D9dWVwC968zMuJrNuKyRN27zviWw9FnxmX16W45Z2-wLZCcQ9nrSH8v8oBCdgHhhmJDZ' --WEBHOOK HERE
+ConfigSv.Webhook = 'WEBHOOK WHERE THE REPORTS ARRIVE' --WEBHOOK
 ConfigSv.FooterImg = "https://cdn.discordapp.com/attachments/900787317684449301/904461451790549002/dani.jpg" --Footer img
 ConfigSv.Command = 'reportar'  --Command for users.
 
